070d56a8cc90897010e72c8266596fd7c8dc5f53
Marek Dohnal <datgaspkitteh@gmail.com>
2023-12-08 17:44:38 +0100

    working branching

--- sketch.js
+++ sketch.js
@@ -5,33 +5,25 @@ function setup() {
 
 function draw() {
   //background(220);
-  let z = random(0,100);
+  let z = random(0, 100);
   let freq = 0.04;
   //drawBark(freq, z);
   //drawNoiseVects();
 
-//for (let i = 0; i < 5; i++) {
-//  let root = new Root(width/2+i*10, height/2, i);
-//  root.draw();
-//}
-let root = new Root(width/2-5, height/2);
-root.draw();
-// root = new Root(width/2, height/2+5);
-// root.draw();
-// root = new Root(width/2+5, height/2);
-// root.draw();
+  let rs = new RootSystem(width / 2 - 5, height / 2, 1, 300, 30);
+  rs.draw();
 }
 
 function drawNoiseVects() {
   let noiseScale = 0.01;
   let step = 50;
-  for (let y = 0; y < height; y+=step) {
-    for (let x = 0; x < width; x+=step) {
+  for (let y = 0; y < height; y += step) {
+    for (let x = 0; x < width; x += step) {
       //console.log("finish");
-      let noiseoff = noise(x*noiseScale, y*noiseScale);
-      let noisea = TAU*noiseoff;
-      x1 = x + cos(noisea)*20;
-      y1 = y + sin(noisea)*20;
+      let noiseoff = noise(x * noiseScale, y * noiseScale);
+      let noisea = TAU * noiseoff;
+      x1 = x + cos(noisea) * 20;
+      y1 = y + sin(noisea) * 20;
 
       circle(x1, y1, 5)
       line(x, y, x1, y1);
@@ -39,104 +31,99 @@ function drawNoiseVects() {
   }
 }
 
-class Root {
-  constructor(x, y) {
+class RootSystem {
+  constructor(x, y, widthVar, maxLen, startSize) {
     this.x = x;
     this.y = y;
-    this.rootLen = 300;
-    this.currLen = 0;
-    this.startSize = 30;
+    this.width = widthVar*PI;
+    this.len = maxLen;
+    this.startSize = startSize;
+    this.rootStack = [];
+
+    let angle = PI/6;
+    let root = new Root(this.x, this.y, this.len, this.startSize, angle);
+    this.rootStack.push(root);
+    angle = this.width/2 - PI/12;
+    root = new Root(this.x, this.y, this.len, this.startSize, angle);
+    this.rootStack.push(root);
+    angle = this.width - PI/6;
+    root = new Root(this.x, this.y, this.len, this.startSize, angle);
+    this.rootStack.push(root);
+  }
+
+  draw() {
+    while (this.rootStack.length > 0) {
+      let root = this.rootStack.pop();
+      let branches = root.draw();
+      if (root.len > 50) {
+        for (var br of branches) {
+          br.len = br.len/3;
+          this.rootStack.push(br);
+        }
+      }
+    }
+  }
+}
+
+class Root {
+  constructor(x, y, len, startSize, angle) {
+    this.x = x;
+    this.y = y;
+    this.len = len;
+    this.startSize = startSize;
     this.segmentSizeX = this.startSize;
     this.segmentSizeY = this.startSize;
-    this.noiseZ = random(0,1);
-    this.offsets = [-1,1]
-    //this.xOrient = random(this.offsets);
-    //this.yOrient = random(this.offsets);
-    this.bx = this.x;
-    this.by = this.y;
-    this.bsgm = this.segmentSize;
-    this.obsgm = this.bsgm;
+    this.noiseZ = random(0, 1);
+    this.angle = angle;
+    this.numBranches = 6;
+    this.noisea = angle;
+  }
+
+  draw() {
+    let branchPoints = [];
+    let nextBrPt = this.len/this.numBranches;
+    for (let i = 0; i < this.len; i++) {
+      this.drawSegment();
+      if (i >= nextBrPt) {
+        branchPoints.push(new Root(this.x, this.y, this.len, this.segmentSizeX, this.noisea));
+        nextBrPt += this.len/this.numBranches;
+      }
+    }
+    return branchPoints;
   }
 
   drawSegment() {
     if (this.segmentSizeX > 1) {
-      stroke(0,100)
+      stroke(0, 100)
       ellipse(this.x, this.y, this.segmentSizeX, this.segmentSizeY);
       this.update();
-      this.currLen++; 
-      // if (this.currLen % 100 == 0) {
-      //   console.log("branchd")
-      //   this.by = this.y;
-      //   this.bx = this.x;
-      //   this.bsgm = this.segmentSizeX;
-      //   this.drawBranch();
-      // }
+      this.currLen++;
     }
   }
 
-  drawBranch() {
-    let cap = random(100,200);
-    for (let i = 0; i < cap; i++) {
-      //stroke(120,100,0,100)
-      circle(this.bx, this.by, this.bsgm);
-      this.updateBranch(cap);
-    }
-  }
- 
   update() {
     let noiseScale = 0.1;
-    let noiseoff = noise(this.x*noiseScale, 
-                          this.y*noiseScale, 
-                          this.noiseZ*noiseScale);
-    let noisea = TAU*noiseoff - 2;
-    let segmSizeOffs = -this.startSize/this.rootLen;
-    console.log(this.segmentSizeX);
+    let noiseoff = noise(this.x * noiseScale,
+      this.y * noiseScale,
+      this.noiseZ * noiseScale);
+    this.noisea = TAU * noiseoff - this.angle;
+    let segmSizeOffs = -this.startSize / this.len;
     this.segmentSizeX += segmSizeOffs;
     this.segmentSizeY += segmSizeOffs;
-    this.x += cos(noisea)*1.5;
-    this.y += sin(noisea)*1.5;
-
-
-    // if (this.currLen > this.rootLen - this.rootLen/5) {
-    //   let numElemsLeft = this.rootLen - this.currLen;
-    //   this.segmentSizeY -= this.segmentSizeY/numElemsLeft;
-    //   this.y += this.segmentSizeY/numElemsLeft;
-    // } else {
-    //   this.y += sin(noisea)*1.5;
-    //   this.segmentSizeY += segmSizeOffs;
-    // }
-  }
-
-  // updateBranch(cap) {
-  //   let noiseScale = 0.1;
-  //   let noiseoff = noise(this.bx*noiseScale, 
-  //                         this.by*noiseScale, 
-  //                         (this.noiseZ+cap)*noiseScale);
-  //   let noisea = TAU*noiseoff - PI;
-  //   //console.log(noisea)
-  //   let segmSizeOffs = -this.bsgm/50;
-  //   this.bsgm += segmSizeOffs;
-  //   this.bx += cos(noisea);
-  //   this.by += sin(noisea);
-  // }
-
-  draw() {
-     
-    for (let i = 0; i < this.rootLen; i++) {
-      this.drawSegment();
-    }
+    this.x += cos(this.noisea) * 1.5;
+    this.y += sin(this.noisea) * 1.5;
   }
 }
 
 function drawBark(freq, z) {
   for (let y = 0; y < height; y++) {
-    let randOffs = map(noise(y*freq),0, 1, 20, 100);
+    let randOffs = map(noise(y * freq), 0, 1, 20, 100);
     for (let x = 0; x < width; x++) {
-      let myNoise = marble(freq*x, freq*y, freq*z);
-      stroke(map(myNoise, 0,1,0,255));
-      
+      let myNoise = marble(freq * x, freq * y, freq * z);
+      stroke(map(myNoise, 0, 1, 0, 255));
+
       if (x > randOffs && x < width - randOffs) {
-       point(x, y);
+        point(x, y);
       }
     }
   }
@@ -152,11 +139,11 @@ function marble(x, y, z) {
   let marble = 0.0;
 
   while (2 * fi <= fs) {
-    marble += ai * noise(fi*x, fi*y, fi*z);
+    marble += ai * noise(fi * x, fi * y, fi * z);
     fi = fi * fm;
     ai = ai * am;
     //console.log(fi);
   }
-  marble = sin(alpha*(x + marble));
+  marble = sin(alpha * (x + marble));
   return marble;
 }
